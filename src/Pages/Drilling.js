c9f3cb6ee680e72d3c17bd804ae8de41b7d733cb
jfrument <92900273+JoeFrumenti@users.noreply.github.com>
2023-10-11 12:22:24 -0700

    made it all an array, added sun image

--- src/Pages/Drilling.js
+++ src/Pages/Drilling.js
@@ -1,22 +1,24 @@
 import React, {useState, useContext} from 'react';
 
 import '../App.css';
-import skeletonImage from '../images/skeleton.jpg'
+import skeletonImage from '../images/skeleton.jpg';
+import sunImage from '../images/sun.jpg';
 
 let context = React.createContext(null);
 function Parent() {
-  const [fName, setfName] = useState(true);
+  const [fName, setfName] = useState(Array(6).fill(false));
 
 
-  function setAllCards()
+  function setAllCardsSkeleton()
   {
-    setfName(!fName);
+    let tempNames = Array(6).fill(true);
+    setfName(tempNames);
   }
 
 
   return (
     <context.Provider value={{ fName }}>
-        <button onClick={setAllCards}> CHANGE THEME </button>
+        <button onClick={setAllCardsSkeleton}>SKELETON</button>
         <CardCollection />
     </context.Provider>
   );
@@ -25,18 +27,22 @@ function Parent() {
 function CardCollection(){
   const cards = [];
 
-  for(let i = 0; i < 3; i++)
+  for(let i = 0; i < 6; i++)
   {
-    cards.push(<Card key = {i} />);
+    cards.push(<Card key = {i} index = {i}/>);
   }
 
-  return(<><div className = "Card-container"> {cards} </div> <div className = "Card-container"> {cards} </div> </>);
+  return(<>
+           <div className = "Card-container"> {cards.slice(0,3)} </div>
+           <div className = "Card-container"> {cards.slice(3)} </div>
+         </>);
 }
 
-function Card() {
+function Card({index}) {
   const { fName } = useContext(context);
   let text;
-  if(fName)
+
+  if(fName[index])
     text = "TRUE";
   else
     text = "FALSE";
